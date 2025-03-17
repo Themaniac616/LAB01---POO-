1be34930e8f498c57b7c0d59372e61ffffa04259
Themaniac616 <165105343+Themaniac616@users.noreply.github.com>
2025-03-16 21:13:46 -0500

    Ejercicios

--- Contenedor.java
+++ Contenedor.java
@@ -2,34 +2,30 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 class ContainerRect {
-    private ArrayList<Rectangulo> rectangulos;
-    private ArrayList<Double> distancias;
-    private ArrayList<Double> areas;
+    private ArrayList<Rectangulo> rectangulos = new ArrayList<>();
+    private ArrayList<Double> distancias = new ArrayList<>();
+    private ArrayList<Double> areas = new ArrayList<>();
     private int capacidad;
 
     public ContainerRect(int capacidad) {
         this.capacidad = capacidad;
-        this.rectangulos = new ArrayList<>();
-        this.distancias = new ArrayList<>();
-        this.areas = new ArrayList<>();
     }
 
     public void addRectangulo(Rectangulo r) {
         if (rectangulos.size() < capacidad) {
             rectangulos.add(r);
-            distancias.add(Coordenada.distancia(r.getEsquina1(), r.getEsquina2()));
+            distancias.add(r.getEsquina1().distancia(r.getEsquina2()));
             areas.add(r.calcularArea());
         } else {
             System.out.println("No se pueden agregar más rectángulos.");
         }
     }
 
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
+    public void mostrarRectangulos() {
+        System.out.println("Rectángulos almacenados:");
         for (int i = 0; i < rectangulos.size(); i++) {
-            sb.append((i + 1) + " " + rectangulos.get(i) + " Distancia: " + distancias.get(i) + " Área: " + areas.get(i) + "\n");
+            System.out.println((i + 1) + " " + rectangulos.get(i) + ", Distancia: " + distancias.get(i) + ", Área: " + areas.get(i));
         }
-        return sb.toString();
     }
 }
 
